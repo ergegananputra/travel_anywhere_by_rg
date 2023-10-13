7e70579950230a7ab50c98c7200021f5d408269e
ergegananputra <ergegananputra@gmail.com>
2023-10-13 17:07:21 +0700

    Interface Jadi

--- app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
+++ app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ppb.travelanywhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ppb.travelanywhere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding){
+
+        }
     }
 }
