2b7cfb3cf990f92f1a1c06a2a08f93f19eddeecf
ergegananputra <ergegananputra@gmail.com>
2023-10-13 21:47:55 +0700

    Done

--- app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
+++ app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ppb.travelanywhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.ppb.travelanywhere.databinding.ActivityMainBinding
@@ -10,6 +12,8 @@ import com.ppb.travelanywhere.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val listTripTanggal = ArrayList<String>()
 
     companion object {
         const val EXTRA_STASIUN_KEBRANGKATAN = "extra_stasiun_keberangkatan"
@@ -40,14 +44,15 @@ class MainActivity : AppCompatActivity() {
             val tanggalKeberangkatan = data?.getStringExtra(EXTRA_TANGGAL_KEBERANGKATAN)
             val namaKereta = data?.getStringExtra(EXTRA_NAMA_KERETA)
             val kelasKereta = data?.getStringExtra(EXTRA_KELAS_KERETA)
-            val paket1 = data?.getStringExtra(EXTRA_PAKET_1)
-            val paket2 = data?.getStringExtra(EXTRA_PAKET_2)
-            val paket3 = data?.getStringExtra(EXTRA_PAKET_3)
-            val paket4 = data?.getStringExtra(EXTRA_PAKET_4)
-            val paket5 = data?.getStringExtra(EXTRA_PAKET_5)
-            val paket6 = data?.getStringExtra(EXTRA_PAKET_6)
-            val paket7 = data?.getStringExtra(EXTRA_PAKET_7)
-            val harga = data?.getStringExtra(EXTRA_HARGA)
+            val paket1 = data?.getBooleanExtra(EXTRA_PAKET_1, false)
+            val paket2 = data?.getBooleanExtra(EXTRA_PAKET_2, false)
+            val paket3 = data?.getBooleanExtra(EXTRA_PAKET_3, false)
+            val paket4 = data?.getBooleanExtra(EXTRA_PAKET_4, false)
+            val paket5 = data?.getBooleanExtra(EXTRA_PAKET_5, false)
+            val paket6 = data?.getBooleanExtra(EXTRA_PAKET_6, false)
+            val paket7 = data?.getBooleanExtra(EXTRA_PAKET_7, false)
+
+            val harga = data?.getIntExtra(EXTRA_HARGA, 0)
 
             // set up
             binding.textUsername.text = "Hi, ${username}!"
@@ -58,6 +63,9 @@ class MainActivity : AppCompatActivity() {
                 && namaKereta != null
                 && kelasKereta != null) {
 
+                // Hilangkan Text Belum Ada Trip
+                binding.textNoTripYet.visibility = View.GONE
+
                 // Tampilkan Card
                 binding.cardViewLastTrip.visibility = View.VISIBLE
 
@@ -67,44 +75,46 @@ class MainActivity : AppCompatActivity() {
                 binding.textKeretaNama.text = namaKereta
                 binding.textKeretaKelas.text = kelasKereta
 
+                listTripTanggal.add(tanggalKeberangkatan)
+
                 // Set Paket
-                if (paket1 == "ACTIVE") {
+                if (paket1 == true) {
                     binding.paket1.visibility = View.VISIBLE
                 } else {
                     binding.paket1.visibility = View.GONE
                 }
 
-                if (paket2 == "ACTIVE") {
+                if (paket2 == true) {
                     binding.paket2.visibility = View.VISIBLE
                 } else {
                     binding.paket2.visibility = View.GONE
                 }
 
-                if (paket3 == "ACTIVE") {
+                if (paket3 == true) {
                     binding.paket3.visibility = View.VISIBLE
                 } else {
                     binding.paket3.visibility = View.GONE
                 }
 
-                if (paket4 == "ACTIVE") {
+                if (paket4 == true) {
                     binding.paket4.visibility = View.VISIBLE
                 } else {
                     binding.paket4.visibility = View.GONE
                 }
 
-                if (paket5 == "ACTIVE") {
+                if (paket5 == true) {
                     binding.paket5.visibility = View.VISIBLE
                 } else {
                     binding.paket5.visibility = View.GONE
                 }
 
-                if (paket6 == "ACTIVE") {
+                if (paket6 == true) {
                     binding.paket6.visibility = View.VISIBLE
                 } else {
                     binding.paket6.visibility = View.GONE
                 }
 
-                if (paket7 == "ACTIVE") {
+                if (paket7 == true) {
                     binding.paket7.visibility = View.VISIBLE
                 } else {
                     binding.paket7.visibility = View.GONE
@@ -116,6 +126,9 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // Menutup Card
                 binding.cardViewLastTrip.visibility = View.GONE
+
+                // Tampilkan Text Belum Ada Trip
+                binding.textNoTripYet.visibility = View.VISIBLE
             }
 
 
@@ -131,7 +144,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding){
+
+            // set nama user
             val username = intent.getStringExtra(RegisterActivity.EXTRA_USERNAME)
+            binding.textUsername.text = "Hi, ${username}!"
+
+            // button pesan
+            binding.buttonPesan.setOnClickListener {
+                val intentToPesanActivity = Intent(this@MainActivity, PesanActivity::class.java)
+                intentToPesanActivity.putExtra(RegisterActivity.EXTRA_USERNAME, username)
+                launcher.launch(intentToPesanActivity)
+            }
+
+            // kalender
+            binding.calendarView.setOnDateChangeListener() {
+                    view,
+                    year,
+                    month,
+                    dayOfMonth ->
+
+                if (listTripTanggal.contains("$dayOfMonth/${month+1}/$year")) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Anda sudah pernah memesan pada tanggal $dayOfMonth/${month+1}/$year",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Anda belum pernah memesan pada tanggal $dayOfMonth/${month+1}/$year",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
 
 
         }
