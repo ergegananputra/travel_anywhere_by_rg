f885b347b2a481b50fe5e23141b5748c21949ff5
ergegananputra <ergegananputra@gmail.com>
2023-12-07 19:46:35 +0700

    UI completed

--- app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
+++ app/src/main/java/com/ppb/travelanywhere/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.ppb.travelanywhere
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ppb.travelanywhere.databinding.ActivityMainBinding
@@ -17,7 +21,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("MainActivity", "onCreate: Started")
+        val typedValue = TypedValue()
+        theme.resolveAttribute(android.R.attr.colorBackground, typedValue, true)
+        val backgroundColor = typedValue.data
+        window.statusBarColor = backgroundColor
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val attributes = window.attributes
+            attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            window.attributes = attributes
+        }
 
         // Bottom Navigation
         with(binding) {
